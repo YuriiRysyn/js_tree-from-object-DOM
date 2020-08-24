b481d2132d5ad575e9db6953ed5de703822fad38
Yurii Rysyn <zerocool_cs@ukr.net>
2020-08-25 00:00:04 +0300

    Updates

---
+++ scripts/main.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const food = {
+  'Meat': {
+    'Pork': {},
+    'Beef': {},
+  },
+
+  'Fruit': {
+    'Red': {
+      'Cherry': {},
+      'Strawberry': {},
+    },
+    'Yellow': {
+      'Banana': {},
+      'Pineapple': {},
+    },
+
+  },
+};
+
+const tree = document.querySelector('#tree');
+
+function createTree(element, data) {
+  // WRITE YOUR CODE HERE
+
+  const ul = document.createElement('ul');
+
+  element.append(ul);
+
+  for (const key in data) {
+    const li = document.createElement('li');
+
+    li.textContent = key;
+
+    ul.append(li);
+
+    if (Object.entries(data[key]).length !== 0) {
+      createTree(li, data[key]);
+    }
+  }
+}
+
+createTree(tree, food);
